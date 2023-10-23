ea524d6afdedc311ffbbdc0f47f857b784e05ef6
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-10-23 13:52:17 +0700

    #9 insert data

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,67 +1,79 @@
 <?php
 
-class Database{
+class Database {
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
     private $db_name = DB_NAME;
 
-    private $dbh; //database handler
-    private $stmt; //statement
+    private $dbh; // database handler
+    private $stmt; // statement
 
     public function __construct()
     {
-      $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
-        $option =[
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ];
-        try{
-           $this ->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-        }catch(PDOException $e){
-            die($e->getMessage());
-        }
+            // data source name
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+
+            // optimasi koneksi ke database
+            $option = [
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ];
+
+            try {
+                $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+            } catch(PDOException $e) {
+                die($e->getMessage());
+            }
     }
 
-    public function query($query){
+    public function query($query)
+    {
         $this->stmt = $this->dbh->prepare($query);
     }
 
-    //binding data
-    public function bind($param, $value, $type = null){
-        if(is_null($type)){
-            switch(true){
-                case is_int ($value) :
+    public function bind($param, $value, $type = null)
+    {
+        // jika type nya null
+        if( is_null($type) ) {
+            // cek tipe data nya
+            switch( true ) {
+                case is_int($value) :
                     $type = PDO::PARAM_INT;
                     break;
-                case is_bool ($value) :
+                case is_bool($value) :
                     $type = PDO::PARAM_BOOL;
                     break;
-                case is_null ($value) :
+                case is_null($value) :
                     $type = PDO::PARAM_NULL;
                     break;
                 default :
                     $type = PDO::PARAM_STR;
             }
         }
-        $this->stmt->bindValue($param,$value,$type);
-    
 
+        $this->stmt->bindValue($param, $value, $type);
     }
 
-
-    public function execute(){
-        $this->stmt->execute();
+    public function execute()
+    {
+            $this->stmt->execute();
     }
 
-    public function resultSet(){
-        $this->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function resultSet()
+    {
+            $this->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function single(){
-       $this->execute();
-       return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    public function single()
+    {
+            $this->execute();
+            return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    public function rowCount()
+    {
+            return $this->stmt->rowCount();
+    }
 }
