7a88178a3c2aab1514ae4f8aee049cf62f5a4a9d
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-10-23 14:38:02 +0700

    #10 Flash Message

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,11 @@
 <div class="container mt-3">
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
+            <?php Flasher::flash() ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
@@ -28,6 +33,7 @@
 
 
 <!-- Modal -->
+
 <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
